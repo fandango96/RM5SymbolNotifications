ebc04dccfeb9626429f3267a0792e078d915d9b2
Abhinav Baid <abhinavbaid@gmail.com>
2021-05-01 13:39:27 +0100

    Implement v1.0 of the app

---
+++ app/src/main/java/io/github/fandango96/rm5symbolnotifications/MainActivity.kt
@@ -0,0 +1,61 @@
+package io.github.fandango96.rm5symbolnotifications
+
+import android.app.AlertDialog
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
+import android.provider.Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS
+import android.text.TextUtils
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Runtime.getRuntime().exec("su")
+
+        if(!isNotificationServiceEnabled()) {
+            buildNotificationServiceAlertDialog().show()
+        }
+    }
+
+    private fun isNotificationServiceEnabled(): Boolean {
+        val pkgName = packageName
+        val flat: String = Settings.Secure.getString(contentResolver,
+                ENABLED_NOTIFICATION_LISTENERS)
+        if (!TextUtils.isEmpty(flat)) {
+            val names = flat.split(":").toTypedArray()
+            for (i in names.indices) {
+                val cn = ComponentName.unflattenFromString(names[i])
+                if (cn != null) {
+                    if (TextUtils.equals(pkgName, cn.packageName)) {
+                        return true
+                    }
+                }
+            }
+        }
+        return false
+    }
+
+    private fun buildNotificationServiceAlertDialog(): AlertDialog {
+        val alertDialogBuilder = AlertDialog.Builder(this).apply {
+            setTitle(R.string.notification_listener_service)
+            setMessage(R.string.notification_listener_service_explanation)
+            setPositiveButton(R.string.yes) { _, _ ->
+                startActivity(Intent(ACTION_NOTIFICATION_LISTENER_SETTINGS))
+            }
+            setNegativeButton(R.string.no) { _, _ ->
+                // If you choose to not enable the notification listener
+                // the app. will not work as expected
+            }
+        }
+        return alertDialogBuilder.create()
+    }
+
+    companion object {
+        private const val ENABLED_NOTIFICATION_LISTENERS = "enabled_notification_listeners"
+    }
+}
---
+++ app/src/main/java/io/github/fandango96/rm5symbolnotifications/NotificationListener.kt
@@ -0,0 +1,140 @@
+package io.github.fandango96.rm5symbolnotifications
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.util.Log
+
+class NotificationListener : NotificationListenerService() {
+    private lateinit var notifications: MutableSet<String>
+    private lateinit var excludedApps: Set<String>
+    private lateinit var excludedCategories: Set<String>
+
+    private var screenOn = true
+
+    private lateinit var screenStateReceiver: BroadcastReceiver
+
+    override fun onListenerConnected() {
+        Log.i(TAG, "Listener connected")
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        notifications = hashSetOf()
+        resources.run {
+            excludedApps = getStringArray(R.array.excludedApps).toSet()
+            excludedCategories = getStringArray(R.array.excludedCategories).toSet()
+        }
+
+        screenStateReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent) {
+                when (intent.action) {
+                    Intent.ACTION_SCREEN_ON -> {
+                        Log.i(TAG, "Screen on")
+
+                        screenOn = true
+
+                        if (notifications.isNotEmpty()) {
+                            stopNotification()
+                        }
+                    }
+                    Intent.ACTION_SCREEN_OFF -> {
+                        Log.i(TAG, "Screen off")
+
+                        screenOn = false
+
+                        if (notifications.isNotEmpty()) {
+                            startNotification()
+                        }
+                    }
+                }
+            }
+        }
+
+        val screenStateReceiverFilter = IntentFilter("screenStateChanged").apply {
+            addAction(Intent.ACTION_SCREEN_ON)
+            addAction(Intent.ACTION_SCREEN_OFF)
+        }
+
+        registerReceiver(screenStateReceiver, screenStateReceiverFilter)
+
+        Log.i(TAG, "Service created")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "Service destroyed")
+        unregisterReceiver(screenStateReceiver)
+    }
+
+    override fun onNotificationPosted(sbn: StatusBarNotification) {
+        sbn.run {
+            val category = sbn.notification.category
+            Log.i(TAG, "Notification posted: $key $isClearable $category")
+
+            if (category != null && !excludedCategories.contains(category) &&
+                    !excludedApps.contains(packageName)) {
+                if (notifications.add(key) && !screenOn) {
+                    startNotification()
+                }
+
+                Log.i(TAG, "Active notification count: ${notifications.size}")
+            } else {
+                Log.i(TAG, "Excluded app/category: $packageName, $category")
+            }
+        }
+    }
+
+    override fun onNotificationRemoved(sbn: StatusBarNotification) {
+        sbn.key.let { key ->
+            Log.i(TAG, "Notification dismissed: $key")
+
+            if (notifications.contains(key)) {
+                notifications.remove(key)
+
+                Log.i(TAG, "Active notification count: ${notifications.size}")
+
+                if (notifications.isEmpty()) {
+                    stopNotification()
+                }
+            }
+        }
+    }
+
+    private fun startNotification() {
+        Log.i(TAG, "Starting symbol notification")
+
+        setSymbolLampEnable(1)
+    }
+
+    private fun stopNotification() {
+        Log.i(TAG, "Stopping symbol notification")
+
+        setSymbolLampEnable(0)
+    }
+
+    private fun setSymbolLampEnable(value: Int) {
+        Runtime.getRuntime().exec(
+                arrayOf(
+                        "su",
+                        "-c",
+                        "content",
+                        "insert",
+                        "--uri",
+                        "content://settings/global",
+                        "--bind",
+                        "name:s:switch_symbol_lamp_enable",
+                        "--bind",
+                        "value:i:$value"
+                )
+        )
+    }
+
+    companion object {
+        private const val TAG = "rm5symbolnotifications"
+    }
+}
